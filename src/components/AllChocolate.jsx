66a539e71fe55323a9d188f39d74b889ede229df
habllu <zahidhasan6999@gmail.com>
2023-11-28 17:00:06 +0600

    simple chocolate-management setup

---
+++ src/components/AllChocolate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllChocolate = () => {
+  return <div>Allllllllllllllllllllllllllllllllllllllllllllllllllllllll</div>;
+};
+
+export default AllChocolate;
---
+++ src/components/NewChocolate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewChocolate = () => {
+  return <div>NewChocolate Mamamaaamamama;</div>;
+};
+
+export default NewChocolate;
